0273814d62c6af11a1980126611cae250770e9ff
Mounir B <40181209+bnmounir@users.noreply.github.com>
2018-12-17 10:43:22 -0500

    Add files via upload

---
+++ context.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+
+const Context = React.createContext();
+
+export class Provider extends Component {
+  state = {
+    contacts: [
+      {
+        id: 1,
+        name: "Bad Boy",
+        email: "bb@gmail.com",
+        phone: "333-333-3333"
+      },
+      {
+        id: 2,
+        name: "Crazy Dog",
+        email: "cd@gmail.com",
+        phone: "555-000-0000"
+      },
+      {
+        id: 3,
+        name: "Storm Fella",
+        email: "SF@gmail.com",
+        phone: "111-111-4444"
+      }
+    ]
+  };
+
+  render() {
+    return (
+      <Context.Provider value={this.state}>
+        {this.props.childern}
+      </Context.Provider>
+    );
+  }
+}
+
+export const Consumer = Context.Consumer;
